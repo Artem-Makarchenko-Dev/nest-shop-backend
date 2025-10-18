1121f868dd03c5a43ad872659d4787819aa98fb2
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2025-10-18 14:58:19 +0300

    Test commit

--- src/auth/dto/refresh-token.dto.ts
+++ src/auth/dto/refresh-token.dto.ts
@@ -1,7 +1,10 @@
-import {IsString, IsNotEmpty} from "class-validator";
+import {
+  IsString,
+  IsNotEmpty
+} from "class-validator";
 
 export class RefreshTokenDto {
   @IsString()
   @IsNotEmpty()
-  refreshToken: string;
+  refreshToken: string
 }
