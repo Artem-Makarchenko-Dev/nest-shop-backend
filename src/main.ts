bc20849b48a30eba363390a21fc22471f02160f9
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2025-10-24 10:37:15 +0300

    Add origin for fixing CORS errors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://artem-makarchenko-next-shop.vercel.app/',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
