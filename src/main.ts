9c6f9552aa6062a41b59d3462d37b3929a9787af
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2025-10-26 15:25:26 +0200

    Updated CORS rules

--- src/main.ts
+++ src/main.ts
@@ -8,27 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
-  const origins =
-    configService.get<string>('CORS_ORIGINS')?.split(',').map(o => o.trim()) ?? [];
-
-  console.log('Allowed origins:', origins);
-
   app.enableCors({
-    origin:[
-      'https://artem-makarchenko-next-shop.vercel.app',
-      'http://localhost:3000',
-    ],
+    origin: 'https://artem-makarchenko-next-shop.vercel.app',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     optionsSuccessStatus: 200,
     preflightContinue: false,
-  });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
-    next();
   });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
