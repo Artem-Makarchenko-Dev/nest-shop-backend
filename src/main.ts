a16cf7f539c8983f61ddc1d82f2713ed35df6b10
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2025-10-26 15:31:40 +0200

    Updated CORS rules

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from "@/src/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
   app.enableCors();
